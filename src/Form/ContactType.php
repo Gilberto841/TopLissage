998eb0632552f4dc0b31674ea0863d59321eeb9c
Gilberto841 <120499584+Gilberto841@users.noreply.github.com>
2024-02-06 18:20:22 +0100

    new style font and home

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -6,7 +6,6 @@ use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\Type;
-use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,11 +14,13 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints\Email;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+       
         // Ajoute les différents champs du formulaire
         $builder
             ->add('name', TextType::class, [
@@ -38,14 +39,14 @@ class ContactType extends AbstractType
             ])
             ->add('email', EmailType::class, [
                 'label' => 'Votre email',
-                'attr' => [
-                    'class' => 'form-control mb-6'
-                ],
                 'constraints' => [
                     new NotBlank(['message' => 'Ce champ ne peut pas être vide.']),
                     new Type(['type' => 'string', 'message' => 'Ce champ doit être une chaîne de caractères.']),
                     new Email()
                 ],
+                'attr' => [
+                    'class' => 'form-control mb-6'
+                ]
             ])
             ->add('phone', TelType::class, [
                 'label' => 'Téléphone',
@@ -86,7 +87,6 @@ class ContactType extends AbstractType
                 'label' => 'Objet',
                 'constraints' => [
                     new NotBlank(['message' => 'Ce champ ne peut pas être vide.']),
-                    new Type(['type' => 'string', 'message' => 'Ce champ doit être une chaîne de caractères.']),
                     new Length([
                         'min' => 10,
                         'max' => 150,
@@ -110,15 +110,13 @@ class ContactType extends AbstractType
                         'max' => 150,
                         'minMessage' => 'Le message doit contenir au moins {{ limit }} caractères.',
                         'maxMessage' => 'Le message ne peut pas dépasser {{ limit }} caractères.'
-                    ]),
-                    new Type(['type' => 'string', 'message' => 'Ce champ doit être une chaîne de caractères.']),
-
+                    ])
                 ],
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Envoyer',
                 'attr' => [
-                    'class' => 'btn btn-primary btn-block mt-4'
+                    'class' => 'btn btn-primary btn-block mt-4 mb-4'
                 ]
             ]);
     }
@@ -128,11 +126,6 @@ class ContactType extends AbstractType
         // Configuration des options du formulaire
         $resolver->setDefaults([
             'data_class' => Contact::class,
-            'csrf_protection' => true,
-
-
-
-            
         ]);
     }
 }
